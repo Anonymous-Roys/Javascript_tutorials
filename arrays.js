406e9d94e3ce24df3082b43be4036b1d7f4fca96
David Arhin <davidarhin2005@gmail.com>
2023-09-15 02:02:55 +0000

    .

--- arrays.js
+++ arrays.js
@@ -20,3 +20,45 @@ function mapUsage(){
     })
     console.log(squaredNum);
 }
+
+
+// Using Map function
+function mapUsage(){
+    const num = [1, 2, 3, 4, 5]
+
+    const squaredNum = num.map(function(n){
+        return n*n
+    })
+    console.log(squaredNum);
+}
+
+
+const transactions = [1000, 3000, 4000, 2000, -898, 3800, -4500];
+var inoDollar = 80;
+
+// using Map method
+function usingMap(){
+    let newTransaction = transactions.map(function(amount){
+        return (amount / 80).toFixed(1)
+    })
+    console.log("$", newTransaction)
+}
+
+// using For Each method
+function usingForEach(){
+    let newTransaction = transactions.forEach(function(amount){
+        console.log((amount / 80).toFixed(1))
+    }) 
+    console.log(newTransaction)
+}
+
+// using reduce
+function reduceMethod() {
+        
+    let reduceMethod = [1, 2, 3, 4, 12]
+    let result = reduceMethod.reduce(function(acc, value){
+        let updatedSum = acc + value
+        return updatedSum;
+    }, 0)
+    console.log(result);
+}
